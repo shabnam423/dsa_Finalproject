bd2ae9fd107bce8b1458a8b4ba26b9141c3f4b16
Nicat-Axundzada <nicataxundzada@gmail.com>
2024-11-20 06:30:33 +0400

    void sortList(T_Student sentinel)

--- dsaproject.c
+++ dsaproject.c
@@ -107,3 +107,25 @@ void deleteLastStudent(T_Student sentinel) {
   free(current->next);
   current->next = NULL;
 }
+
+void sortList(T_Student sentinel) {
+  T_Student sorted = NULL;
+
+  T_Student current = sentinel->next;
+  while(current) {
+    T_Student next = current->next;
+    if (sorted == NULL || sorted->id_num >= current->id_num) {
+      current->next = sorted;
+      sorted = current;
+    } else {
+      T_Student temp = sorted;
+      while (temp->next != NULL && temp->next->id_num < current->id_num) {
+        temp = temp->next;
+      }
+    current->next = temp->next;
+    temp->next = current;
+    }
+    current = next;
+  } 
+  sentinel->next = sorted;
+}
